d8f1d7c0299a83f2a3d24e75061f443499e204f3
Isoga88 <126089080+Isoga88@users.noreply.github.com>
2023-05-08 16:16:30 +0200

    2 files modified, 3 deleted

--- src/root/root.component.ts
+++ src/root/root.component.ts
@@ -1,14 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { AcquisizioneComponent } from './acquisizione/acquisizione.component';
 import { RicercaComponent } from './ricerca/ricerca.component';
+import { CommonModule } from '@angular/common';
+
 @Component({
   selector: 'app-root',
   templateUrl: './root.component.html',
   styleUrls: ['./root.component.css'],
-  imports: [RicercaComponent, AcquisizioneComponent],
+  imports: [CommonModule, RicercaComponent, AcquisizioneComponent],
   standalone: true,
 })
 export class RootComponent implements OnInit {
+  visita: boolean = true;
   constructor() {}
 
   ngOnInit() {}
