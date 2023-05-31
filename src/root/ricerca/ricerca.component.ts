4db1de9bbd4823b72303a04691a81a0afd77ad18
Isoga88 <126089080+Isoga88@users.noreply.github.com>
2023-05-31 15:28:46 +0200

    1 file modified

--- src/root/ricerca/ricerca.component.ts
+++ src/root/ricerca/ricerca.component.ts
@@ -18,25 +18,33 @@ export class RicercaComponent implements OnInit {
   @Output() cambioVista = new EventEmitter<number>;
 
   libroScelto : Libro
+  occorrenze: number = 0;
+
   cambiaVista(numero: number) {
     this.visita = numero;
     this.cambioVista.emit(this.visita);
+    this.occorrenze = 0
   }
-  occorrenze: number = 0;
   cercaLibro() {
     let libri = this.mioArchivio.archivio
     let nodoSequenza: HTMLInputElement = document.getElementById(
       'cerca'
     ) as HTMLInputElement;
     let sequenza = nodoSequenza.value;
-    const libriCorrispondenti = libri.filter((libro) =>
+    let libriCorrispondenti:Array<Libro>
+    if (sequenza == " "){
+      libriCorrispondenti = []
+    }else{
+      libriCorrispondenti = libri.filter((libro) =>
       (libro.titolo + libro.autore).toLowerCase().includes(sequenza)
     );
+    }
     if (libriCorrispondenti.length === 1) {
       this.cambiaVista(3)
       this.libroScelto = libriCorrispondenti[0]      
     } else {
       this.occorrenze = libriCorrispondenti.length;
+      console.log(libriCorrispondenti)
     }
   }
   constructor(private db: DatabaseService) {}
